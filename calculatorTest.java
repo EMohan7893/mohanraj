f995c1083343d42975e04ef14e9608f5bf7d0ef3
EMohan7893 <mohanraj4817@gmail.com>
2025-08-29 14:57:13 +0530

    Update calculatorTest.java

--- calculatorTest.java
+++ calculatorTest.java
@@ -2,7 +2,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
-    Calculator calc = new Calculator();
+    Calculator calc = new calculator();
     @Test public void testAddition() { assertEquals(15, calc.add(10, 5)); }
     @Test public void testSubtraction() { assertEquals(5, calc.subtract(10, 5)); }
     @Test public void testMultiplication() { assertEquals(50, calc.multiply(10, 5)); }
