d8fcc61e54f1650e9f4a460fe086be5ced2f3eeb
EMohan7893 <mohanraj4817@gmail.com>
2025-08-29 14:37:24 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,9 +1,23 @@
 public class calculator{
-public int add(int a,int b){return a+b}
-public int substract (int a,int b){return a-b}
-public int multiply(int a,int b){return a*b}
-public int divide(int a,int b){if(b==0) throw new
-Arithmetic Expection("cannot divide by zero"):return a/b;}
+public int add(int a,int b)
+  {
+    return a+b;
+  }
+public int substract (int a,int b)
+  {
+    return a-b;
+  }
+public int multiply(int a,int b)
+  {
+    return a*b;
+  }
+public int divide(int a,int b)
+  {
+    if(b==0)
+      throw new
+Arithmetic Expection("cannot divide by zero")
+        return a/b;
+  }
 public Static void main(String[]args)
 {
 calculator calc= new calculator();
