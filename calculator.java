24af4ca4209609663442c1b1b024e711ab17bd00
mca <mca@L1-45.ssccdomain.in>
2025-08-29 12:04:05 +0530

    initial commit

---
+++ calculator.java
@@ -0,0 +1,17 @@
+public class calculator{
+public int add(int a,int b){return a+b}
+public int substract (int a,int b){return a-b}
+public int multiply(int a,int b){return a*b}
+public int divide(int a,int b){if(b==0) throw new
+Arithmetic Expection("cannot divide by zero"):return a/b;}
+public Static void main(String[]args)
+{
+calculator calc= new calculator();
+System.out.println("Addition"+calc.add(10,5));
+System.out.println("subtraction"+calc.substract(10,5));
+System.out.println("multiplication:"+calc.multiply(10,5));
+System.out.println("Division:"+calc.divide(10,5))
+
+
+
+
---
+++ calculatorTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.assert equals;
+public class calculatorTest{
+calculator calc=new calculator();
+@Test public void test addition(){assert Equals(15,cal.add(10.5));}
+@Test public void test substration(){assert Equals (5,calc substract(10,5));}
+@Test public void test Multipication(){assert Equals(50,calc.Multiply(10,5));)
+@Test public void test Division(){assert Equals(divide(10,5));}}
+
