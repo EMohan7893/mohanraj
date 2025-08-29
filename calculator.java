8989b5aeef78f22ce9544cb630ffb2cc25aa336b
EMohan7893 <mohanraj4817@gmail.com>
2025-08-29 14:42:54 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -12,20 +12,20 @@ public int multiply(int a,int b)
   {
     return a*b;
   }
-public int divide(int a,int b)
-  {
-    if(b==0)
-      throw new Arithmetic Expection("cannot divide by zero")
-        
-        return a/b;
-  }
+public int divide(int a, int b) {
+    if (b == 0) {
+        throw new ArithmeticException("Cannot divide by zero");
+    }
+    return a / b;
+}
+
 public static void main(String[]args)
 {
 calculator calc= new calculator();
 System.out.println("Addition"+calc.add(10,5));
 System.out.println("subtraction"+calc.substract(10,5));
 System.out.println("multiplication:"+calc.multiply(10,5));
-System.out.println("Division:"+calc.divide(10,5))
+System.out.println("Division:"+calc.divide(10,5));
 
 
 
